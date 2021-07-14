54b0476f98b664802a4ad6b8319d7a66c740b5d3
Sanzaka <equation0815@icloud.com>
2021-07-14 05:28:06 +0000

    [add]大半のviewページ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,4 +11,18 @@ class UsersController < ApplicationController
     @books = @user.books
   end
   
+  def edit
+    @user = User.find(current_user.id)
+  end
+  
+  def update
+    @user = User.find(current_user.id)
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'ad177f108dc0b91175c368256bc9f973ecd6ac1fb145d6b0a558c54c1d30155bba1122e49fbee30a6932d313c39ace8d24a1b211a0f79e9d5b426a27129db322'
